41aa875dda70a6a66e790094cf9ae237d5a8bc0e
Oleg Butuzov <butuzov@made.ua>
2021-08-30 21:37:24 +0300

    refactor: stdlib generator

--- scripts/generate-std.sh
+++ scripts/generate-std.sh
@@ -26,12 +26,9 @@ for i in $(seq 2 17); do
     printf "\t// added in Go v1.%s in compare to v1.%s (docker image)\n" $i "$(($i-1))" >> $dest
 
     for pkg in $(comm -13 <(sort .tmp/go@1.$(($i-1))) <(sort .tmp/go@1.$i)); do
-        printf "\t\"%s\": struct{}{},\n" $(echo $pkg | tr -d \\r) >> $dest
+        printf "\t\"%s\":  {},\n" $(echo $pkg | tr -d \\r) >> $dest
     done
 
-    # whats removed in new release not really required.
-    # printf "(removed) Go v1.%s vs v1.%s\n" $i "$(($i-1))"
-    # comm -13  <(sort go@1.$i) <(sort go@1.$(($i-1)))
 done
 
 echo "}" >> $dest
