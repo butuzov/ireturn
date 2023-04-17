08b25fe89d5e9abbfb99e169a986aec454c07c75
Oleg Butuzov <butuzov@made.ua>
2023-04-17 21:32:42 +0300

    fix: update stdlib packages

--- scripts/generate-std.sh
+++ scripts/generate-std.sh
@@ -3,7 +3,7 @@
 mkdir -p .tmp
 
 # create std pkg list
-for i in $(seq 1 17); do
+for i in $(seq 1 20); do
     docker run --rm -it golang:1.$i go list std \
         | grep -v internal | grep -v vendor > .tmp/go@1.$i;
     docker rmi golang:1.$i
@@ -21,7 +21,7 @@ echo "package analyzer" >> $dest
 echo "" >> $dest
 echo "var std = map[string]struct{}{" >> $dest
 
-for i in $(seq 2 17); do
+for i in $(seq 2 20); do
 
     printf "\t// added in Go v1.%s in compare to v1.%s (docker image)\n" $i "$(($i-1))" >> $dest
 
