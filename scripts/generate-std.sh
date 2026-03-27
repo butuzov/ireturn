b30c38bf58a2531787219371c1fc1258cede2a00
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-03-27 11:18:36 +0100

    chore: update std

--- scripts/generate-std.sh
+++ scripts/generate-std.sh
@@ -2,16 +2,11 @@
 
 mkdir -p .tmp
 
-tip=24
+tip=26
 
 # create std pkg list
-for i in $(seq 1 $tip); do
-    if [[ ! -a .tmp/go@1.$i ]]; then
-        docker run --rm -it golang:1.$i go list std \
-            | grep -v internal | grep -v vendor > .tmp/go@1.$i;
-        docker rmi golang:1.$i
-    fi
-done
+
+docker run --rm -it golang:1.${tip}-alpine go list std | grep -v internal | grep -v vendor > .tmp/go@1.${tip};
 
 dest="analyzer/std.go"
 
@@ -25,14 +20,8 @@ echo "package analyzer" >> $dest
 echo "" >> $dest
 echo "var std = map[string]struct{}{" >> $dest
 
-for i in $(seq 2 $tip); do
-
-    printf "\t// added in Go v1.%s in compare to v1.%s (docker image)\n" $i "$(($i-1))" >> $dest
-
-    for pkg in $(comm -13 <(sort .tmp/go@1.$(($i-1))) <(sort .tmp/go@1.$i)); do
-        printf "\t\"%s\":  {},\n" $(echo $pkg | tr -d \\r) >> $dest
-    done
-
+for pkg in $(sort .tmp/go@1.${tip}); do
+    printf "\t\"%s\":  {},\n" "$(echo "$pkg" | tr -d \\r)" >> $dest
 done
 
 echo "}" >> $dest
